2d36add11204a98115d29fd473e14a3f710415b2
zack <zack@users.noreply.github.com>
2026-03-19 22:43:21 +0100

    Add DINO-WM project source files

---
+++ models/dino.py
@@ -0,0 +1,27 @@
+import torch
+import torch.nn as nn
+
+torch.hub._validate_not_a_forked_repo=lambda a,b,c: True
+
+class DinoV2Encoder(nn.Module):
+    def __init__(self, name, feature_key, repo_ref="ebc1cba"):
+        super().__init__()
+        self.name = name
+        self.repo_ref = repo_ref
+        self.base_model = torch.hub.load(f"facebookresearch/dinov2:{repo_ref}", name)
+        self.feature_key = feature_key
+        self.emb_dim = self.base_model.num_features
+        if feature_key == "x_norm_patchtokens":
+            self.latent_ndim = 2
+        elif feature_key == "x_norm_clstoken":
+            self.latent_ndim = 1
+        else:
+            raise ValueError(f"Invalid feature key: {feature_key}")
+
+        self.patch_size = self.base_model.patch_size
+
+    def forward(self, x):
+        emb = self.base_model.forward_features(x)[self.feature_key]
+        if self.latent_ndim == 1:
+            emb = emb.unsqueeze(1) # dummy patch dim
+        return emb
